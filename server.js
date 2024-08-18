60ceac29719adb27c4292404a3bd48e24d16639b
Juma Harun <mmbh4run@gmail.com>
2024-08-18 18:54:32 +0300

    set up basic server

--- server.js
+++ server.js
@@ -1,2 +1,15 @@
 // @ts-check
-console.log("Hello, World!");
+const express = require("express");
+
+require("dotenv").config();
+
+const app = express();
+const port = process.env.PORT;
+
+app.get("/", (req, res) => {
+  res.send("Hello, World!");
+});
+
+app.listen(port, () => {
+  console.log(`Server running on port: ${port}`);
+});
