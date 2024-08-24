3015d9a8df4494b1a31806104072a47b130b9e0f
Juma Harun <mmbh4run@gmail.com>
2024-08-24 19:08:25 +0300

    Added tests for GET /api/posts/:id endpoint

--- src/__tests__/postController.test.ts
+++ src/__tests__/postController.test.ts
@@ -45,3 +45,34 @@ describe("GET /posts", () => {
     expect(response.body[2].title).toBe(post2.title);
   });
 });
+
+describe("GET /posts/:id", () => {
+  beforeAll(async () => {
+    await mongoose.connect("mongodb://localhost:27017/testDB");
+  });
+
+  afterEach(async () => {
+    await Post.deleteMany({});
+  });
+
+  afterAll(async () => {
+    await mongoose.connection.close();
+  });
+
+  it("should find and return a specific post using its _id", async () => {
+    const post1 = new Post({
+      title: "Post 1",
+      content: "This is content for Post 1",
+      author: "Author 1",
+    });
+
+    await post1.save();
+
+    const response = await request(app).get(`/api/posts/${post1._id}`);
+
+    expect(response.status).toBe(200);
+    expect(response.body.title).toBe(post1.title);
+    expect(response.body.author).toBe(post1.author);
+    expect(response.body.content).toBe(post1.content);
+  });
+});
